df60ba9a3b7bac066b691e263a4d589e3fb2f020
cmolarti <cmolarti@gmail.com>
2023-12-17 23:32:21 -0500

    create static folder for images, include new webpages, include url_for

--- weatherAPI.py
+++ weatherAPI.py
@@ -2,9 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/home')
 def home():
     return render_template('tutorial.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
-app.run()
+@app.route('/contactus')
+def contactus():
+    return render_template('contactus.html')
+
+app.run(debug=True)
