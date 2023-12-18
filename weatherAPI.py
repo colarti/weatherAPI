93861fced7e0b605d514fe0c0e16e05fcbf4a5c0
cmolarti <cmolarti@gmail.com>
2023-12-17 23:53:00 -0500

    start creating an api using home.html, also create a basic query req

--- weatherAPI.py
+++ weatherAPI.py
@@ -1,17 +1,17 @@
 from flask import Flask, render_template
+import pandas
 
 app = Flask(__name__)
 
-@app.route('/home')
+@app.route('/')
 def home():
-    return render_template('tutorial.html')
+    return render_template('home.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-@app.route('/contactus')
-def contactus():
-    return render_template('contactus.html')
+@app.route('/api/v1/<station>/<date>')
+def about(station, date):
+    # df = pandas.read_csv('')
+    # temp = df.station(date)
+    temp = 23
+    return {'station':station, 'date':date, 'temp':temp}
 
 app.run(debug=True)
